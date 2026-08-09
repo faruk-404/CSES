8ec17077b9f2aca47122b26a448ae14aece07452
Md Faruk Hossain <faruk.x.404@gmail.com>
2026-08-09 18:25:00 +0600

    09/08/2026

--- Factory_Machines.cpp
+++ Factory_Machines.cpp
@@ -2,43 +2,93 @@
 using namespace std;
 
 #define nl '\n'
-#define nf cout<<'\n'
+#define nf cout << '\n'
 #define int long long
 #define cy cout << "YES\n"
 #define cn cout << "NO\n"
-#define all(v) v.begin(),v.end()
-#define rall(v) v.rbegin(),v.rend()
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 
-bool ok(int mid,vector<int> a,int t){
-    int cnt=0;
-    for(int i=0;i<(int)a.size();i++){
-        cnt+=(mid/a[i]);
-        if((cnt>=t))return true;
-    }
-    return (cnt>=t);
-}
-void solve(){
-    int n,t;cin>>n>>t;
+void solve() {
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n);
-    for(auto &i:a)cin>>i;
-    int l=0,r=1e18,mid,ans=0;
-    while(l<=r){
-        mid=l+(r-l)/2;
-        if(ok(mid,a,t)){
-            ans=mid;
-            r=mid-1;
+    for (auto &i : a)
+        cin >> i;
+    auto ok = [&](int mid) {
+        int cnt = 0;
+        for (auto i : a) {
+            cnt += (mid / i);
+            if (cnt >= k) {
+                break;
+            }
         }
-        else {
-            l=mid+1;
+        return (cnt >= k);
+    };
+
+    int l = 0, r = 1e18, mid = 0, ans = 0;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (ok(mid)) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
         }
     }
-    cout<<ans<<nl;
+
+    cout << ans << nl;
 }
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t=1;
-    // cin>>t;
-    while(t--){solve();}
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
-}
+} // #include <bits/stdc++.h>
+// using namespace std;
+
+// #define nl '\n'
+// #define nf cout<<'\n'
+// #define int long long
+// #define cy cout << "YES\n"
+// #define cn cout << "NO\n"
+// #define all(v) v.begin(),v.end()
+// #define rall(v) v.rbegin(),v.rend()
+
+// bool ok(int mid,vector<int> a,int t){
+//     int cnt=0;
+//     for(int i=0;i<(int)a.size();i++){
+//         cnt+=(mid/a[i]);
+//         if((cnt>=t))return true;
+//     }
+//     return (cnt>=t);
+// }
+// void solve(){
+//     int n,t;cin>>n>>t;
+//     vector<int> a(n);
+//     for(auto &i:a)cin>>i;
+//     int l=0,r=1e18,mid,ans=0;
+//     while(l<=r){
+//         mid=l+(r-l)/2;
+//         if(ok(mid,a,t)){
+//             ans=mid;
+//             r=mid-1;
+//         }
+//         else {
+//             l=mid+1;
+//         }
+//     }
+//     cout<<ans<<nl;
+// }
+// int32_t main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int t=1;
+//     // cin>>t;
+//     while(t--){solve();}
+//     return 0;
+// }
